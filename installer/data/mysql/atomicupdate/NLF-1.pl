ecbf2e6a05782a8b130fbb27df8339c3673332bc
Ere Maijala <ere.maijala@helsinki.fi>
2017-11-07 11:20:16 +0200

    NLF-1 Fix the atomic update.

--- installer/data/mysql/atomicupdate/NLF-1.pl
+++ installer/data/mysql/atomicupdate/NLF-1.pl
@@ -10,7 +10,5 @@ unless ($atomicUpdater->find('NLF-1')) {
         ALTER TABLE search_field CHANGE COLUMN type type ENUM('', 'string', 'date', 'number', 'boolean', 'sum', 'isbn', 'stdno') NOT NULL COMMENT 'what type of data this holds, relevant when storing it in the search engine'
     });
 
-$dbh->do("INSERT INTO atomicupdates (issue_id, filename) VALUES ('Bug14698', 'Bug14698-AtomicUpdater.pl')");
-
     print "Upgrade done (NLF-1: update search_field table enum)\n";
 }
